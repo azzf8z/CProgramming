511efe047f61aeafef1736180312dc32df9a5d09
azzf8z <jperera3465@floridapoly.edu>
2026-01-14 14:50:27 +0000

    first commit

---
+++ 01_programing/1st_program.cpp
@@ -0,0 +1,23 @@
+/*
+The first C++ Program 
+01/14/2026
+*/
+
+#include<iostream>
+
+//create main function
+int main(void) {
+    
+    // std - standard namespace
+    // cout - console output (object of iostream class)
+    // << - stream insertion operator
+    //endl - end line (move the cursor in the terminal to the next line)
+    // ctrl b - hides the explorer
+
+
+    std::cout << "Hello from COP3371!\n" << std:: endl;
+
+    std:: cout << "(15 * 78) + 14 is " << (15 * 78) + 14 << std::endl;
+    
+    return 0;
+}

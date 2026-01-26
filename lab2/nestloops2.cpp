2e24c44bb46ab292f6f586391decb5620069a616
azzf8z <jperera3465@floridapoly.edu>
2026-01-26 03:47:37 +0000

    idk

--- lab2/nestloops2.cpp
+++ lab2/nestloops2.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
-#include<iomanip>
-
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main (void) {
+int main() {
 
     int n;
-    
-    cout << "Enter a number from 1 to 15: " << endl;
+    cout << "Enter the number of lines (from 1 to 15): ";
     cin >> n;
 
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++) {
 
-        for(int j = 0; j < n - i ; j++){
-            cout << setw(3) << " ";
+        for (int j = 0; j < n - i; j++)
+            cout << "    ";
+        
+
+        int total = 2 * i - 1;
+
+        for (int k = 0; k < total; k++) {
+            int val = (k < i) ? (i - k) : (k - i + 2);
+
+            cout << left << setw(2) << val;
+
+            if (k != total - 1) cout << "  ";
         }
 
-        for (int p = i; p >= 1; p--){
-            cout << setw(3) << p;
-        }
-
-        for(int t = 2; t <= i; t++){
-            cout << setw(3) << t;
-        }
-
-        cout << endl;
+        cout << '\n';
     }
-    
+
     return 0;
 }

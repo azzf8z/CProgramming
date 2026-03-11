90fe891c35ab9e738c659e48554a3339f74fab54
azzf8z <jperera3465@floridapoly.edu>
2026-03-11 04:41:03 +0000

    added dynamic array

--- 19_myvector/myvector.cpp
+++ 19_myvector/myvector.cpp
@@ -29,9 +29,21 @@ void MyVector::print() const {
     }
     std::cout << "]\n";
 }
-    
+
 void MyVector::push_back(int val){
-    //todo make it dynamic array
+    if(size == capacity){ //if array is full
+        capacity *= 2; //double its size
+
+        int* newArr = new int[capacity]; //create a new array in the heap with the larger capacity
+
+        for(int i = 0; i < size; i++){//iterate through the old array
+            newArr[i] = elements[i];//copy each element into new array
+        }
+
+        delete[] elements;   //allocate old array to free up space no longer used
+        elements = newArr; // point to new array with larger size
+    }
+
     elements[size] = val;
-    size++;
-    };
+    size++; //increase size because it may contain one more element
+}
